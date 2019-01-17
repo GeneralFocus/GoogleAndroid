a1e3a83c99a162e4a085669285932a9a4ffd10ea
Oluwole <oluwole.majiyagbe@britecore.com>
2019-01-17 21:55:23 +0100

    Update Login

--- app/src/main/java/ng/nexttrip/nexttrip2/signin/AuthenticationActivity.java
+++ app/src/main/java/ng/nexttrip/nexttrip2/signin/AuthenticationActivity.java
@@ -1,6 +1,7 @@
 package ng.nexttrip.nexttrip2.signin;
 
 import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
@@ -12,19 +13,24 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.jetbrains.annotations.NotNull;
 
 import java.util.concurrent.TimeUnit;
 
+import ng.com.maktay.nexttrip.signup.Registration;
 import ng.nexttrip.nexttrip2.PhoneActivity;
 import ng.nexttrip.nexttrip2.R;
 import ng.nexttrip.nexttrip2.home.HomeActivity;
+import ng.nexttrip.nexttrip2.signup.RegInterface;
 import ng.nexttrip.nexttrip2.signup.RegistrationActivity;
 
 /**
  * Created by Olabode Qudus on 11/10/2018.
  */
 
-public class AuthenticationActivity extends AppCompatActivity {
+public class AuthenticationActivity extends AppCompatActivity implements RegInterface.View {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +43,43 @@ public class AuthenticationActivity extends AppCompatActivity {
         Intent intent = new Intent(AuthenticationActivity.this, HomeActivity.class);
         startActivity(intent);
         overridePendingTransition(R.anim.slide_in, R.anim.slide_in);
+    }
+
+    void login(String phoneNumber){
+        //TODO: Call this method to Login
+        Registration presenter = new Registration(this);
+        presenter.login(phoneNumber);
+    }
+
+    @Override
+    public void showError(@NotNull String error) {
+        Toast.makeText(this, error, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void showMessage(@NotNull String message) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void openRegister() {
+        startActivity(new Intent(AuthenticationActivity.this, RegistrationActivity.class));
+    }
+
+    @Override
+    public void openOTP() {
+        //TODO: Open OTP Activity
+    }
+
+    @Override
+    public void showProgress(boolean show) {
+        //TODO: Show Progress
+    }
+
+    @NotNull
+    @Override
+    public Context getContext() {
+        return this;
     }
 
 }
