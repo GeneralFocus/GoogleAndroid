90a77037a62015e0921ac2474b584246381a856f
generalfocus <generalfocus1@gmail.com>
2019-01-21 21:50:36 +0100

    initial commit

---
+++ app/src/main/java/ng/nexttrip/nexttrip2/signup/DriverRegistrationActivity.java
@@ -0,0 +1,13 @@
+package ng.nexttrip.nexttrip2.signup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DriverRegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_registration);
+    }
+}
